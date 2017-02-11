0dbcd4f3d8528f6dc216e3d99d25c7a92ebf8776
Dustin <dustin@urbanstems.com>
2017-02-11 14:27:16 -0500

    Add test setup

---
+++ test/example.test.js
@@ -0,0 +1,8 @@
+const { expect } = require('chai');
+const { describe, it } = require('mocha');
+
+describe('Somethang...', () => {
+  it('should pass', () => {
+    expect(true).to.be.true;
+  });
+});
